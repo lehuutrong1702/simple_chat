c9193eb2dd3c9399a9b55caa22e92a5045f25829
trong <lehuutrong141@gmail.com>
2024-09-20 19:42:18 +0700

    get messages from firebase

--- lib/widgets/chat_message.dart
+++ lib/widgets/chat_message.dart
@@ -1,15 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
   const ChatMessages({super.key});
 
-  
-
   @override
   Widget build(BuildContext context) {
-      return Center(
-        child: Text("No message found."),
-      ) ;
+    return StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chat')
+            .orderBy('createdAt', descending: false)
+            .snapshots(),
+        builder: (ctx, chatSnapshots) {
+          if (chatSnapshots.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (!chatSnapshots.hasData || chatSnapshots.data!.docs.isEmpty) {
+            return const Center(
+              child: Text('No message found.'),
+            );
+          }
+          final loadedMessages = chatSnapshots.data!.docs;
+          return ListView.builder(
+              itemCount: loadedMessages.length,
+              itemBuilder: (ctx, index) {
+                return Text(
+                  loadedMessages[index].data()['text'],
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                );
+              });
+        });
   }
-  
 }
