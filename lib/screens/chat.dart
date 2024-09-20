c9ea3e191f851040c865b6cec41a4809c00c70d5
trong <lehuutrong141@gmail.com>
2024-09-20 18:26:06 +0700

    add message and new message widget

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:simple_chat/widgets/chat_message.dart';
+import 'package:simple_chat/widgets/new_message.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -7,20 +10,22 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Chat screen"),
-        actions: [
-          IconButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              icon: Icon(Icons.exit_to_app,
-                  color: Theme.of(context).colorScheme.primary))
-        ],
-      ),
-      body: const Center(
-        child: Text("logged in!"),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Chat screen"),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                },
+                icon: Icon(Icons.exit_to_app,
+                    color: Theme.of(context).colorScheme.primary))
+          ],
+        ),
+        body: Column(
+          children: [
+            Expanded(child: ChatMessages()),
+            NewMessage(),
+          ],
+        ));
   }
 }
